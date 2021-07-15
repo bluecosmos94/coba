4b90233b62f6b807145741e7ffcc720c0b704d8e
Kelny Tan <bluecosmos94@gmail.com>
2021-07-15 16:35:05 +0700

    Initial commit

---
+++ coba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coba
+//
+//  Created by Kelny Tan on 15/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coba/cobaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cobaApp.swift
+//  coba
+//
+//  Created by Kelny Tan on 15/07/21.
+//
+
+import SwiftUI
+
+@main
+struct cobaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
